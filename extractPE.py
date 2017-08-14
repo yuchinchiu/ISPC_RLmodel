4365e3789c0acd45a0307f589bb3d5519100cff5
yuchinchiu <chiu.yuchin@gmail.com>
2017-08-14 12:12:51 -0400

    First commit

--- extractPE.py
+++ extractPE.py
@@ -9,8 +9,8 @@ import os
 import pandas as pd
 import numpy as np
 
-#workingDir = os.path.dirname(os.path.realpath(__file__))
-#os.chdir(workingDir)
+workingDir = os.path.dirname(os.path.realpath(__file__))
+os.chdir(workingDir)
 
 def conflictPred(c,alpha):
     # regurn trial-by-trial conflict prediction based on a given conflict sequence and learning rate- alpha
@@ -20,61 +20,68 @@ def conflictPred(c,alpha):
         p[i+1] = p[i]*(1-alpha) + c[i]*alpha
     return p
 
-
 colNames = ['sbjId','faceId','nameId','conflict','zRT','RT']
 df_group = pd.read_csv('ISPC_behavior.csv', header=0, names=colNames)
 df_gp_wCP = pd.DataFrame()
+gp_alphas = pd.DataFrame()
 
-gp_alphas=pd.DataFrame()
+SCNT=-1
 
-
-S = 1
-SCNT=0
-df = df_group.loc[df_group.sbjId==S,:]
-idxC = df[(df.conflict==0) & (df.zRT.notnull())].index
-idxI = df[(df.conflict==1) & (df.zRT.notnull())].index
-bestSSE = -1
-
-for alpha_face in np.arange(0.01,1,0.01):
-    for alpha_name in np.arange(0.01,1,0.01):
+for S in np.unique(df_group.sbjId):
+    SCNT = SCNT+1    
+    df = df_group.loc[df_group.sbjId==S,:]
+    
+    # for later Design matrix construction
+    idxC = df[(df.conflict==0) & (df.zRT.notnull())].index
+    idxI = df[(df.conflict==1) & (df.zRT.notnull())].index
+    RT = np.concatenate((np.array(df.loc[idxC,'RT']), np.array(df.loc[idxI,'RT'])), axis=0)
+    constC = np.transpose(np.array([[1],[0],[0],[0]])*np.ones(len(idxC),dtype=float))
+    constI = np.transpose(np.array([[1],[0],[0],[0]])*np.ones(len(idxI),dtype=float))    
+    bestSSE = -1
+    
+    # figure out each face/name's conflict trial sequence ahead of time for the loop
+    conflict_face = []
+    conflict_name = []
+    for ID in range(1,9,1):  # for faceid/nameid 1-8
+        conflict_face.append(df.loc[df.faceId==ID, 'conflict'])
+        conflict_name.append(df.loc[df.nameId==ID, 'conflict'])
         
+    # Run exhaustive search for the best fitting alpha_face, alpha_name using RT data
+    for alpha_face in np.arange(0.01, 1, 0.01):
+        for alpha_name in np.arange(0.01, 1, 0.01):
         # plug in the alphas and derive CP_face and CP_name
-        for f in np.unique(df.faceId):
-            c = df.loc[df.faceId==f, 'conflict']
-            p = conflictPred(c, alpha_face)
-            df.loc[df.faceId==f, 'CP_face'] = p
+            CP_face=[]
+            CP_name=[]
+            for i in range(0,8,1):  # 0-7 (8 unique ids)
+                CP_face.append(conflictPred(conflict_face[i], alpha_face))
+                CP_name.append(conflictPred(conflict_name[i], alpha_name))
+            # put CP back to the original df
+            for i in range(0,8,1):
+                df.loc[df[df.faceId==i+1].index,'CP_face']=CP_face[i]
+                df.loc[df[df.nameId==i+1].index,'CP_name']=CP_name[i]
             
-        for n in np.unique(df.nameId):
-            c = df.loc[df.nameId==n, 'conflict']
-            p = conflictPred(c, alpha_name)
-            df.loc[df.nameId==n, 'CP_name'] = p            
+            # Construct design matrix and calculate the Sum of squared errors (SSEs) 
+            dmC = np.array(df.loc[idxC,['CP_face','CP_name']])        
+            dmC = np.concatenate((dmC, constC), axis=1)
+            dmI = df.loc[idxI,['CP_face','CP_name']]        
+            dmI = np.concatenate((dmI, constI), axis=1)
+            dmI = dmI[:,[3,4,5,0,1,2]]
+            DM = np.concatenate((dmC, dmI),axis=0)
+        
+            diff =  RT - np.dot(np.dot(DM, np.linalg.pinv(DM)),RT)
+            SSE = np.square(diff).sum()
             
-        # Construct design matrix and calculate the Sum of squared errors (SSEs) 
-        dmC = df.loc[idxC,['CP_face','CP_name']]
-        dmC['constant'] = 1
-        dmI = df.loc[idxI,['CP_face','CP_name']]
-        dmI['constant'] = 1        
-        dmC = np.array(dmC)
-        dmI = np.array(dmI)
-        RT1 = np.array(df.loc[idxC,'RT'])
-        diffC =  RT1 - np.dot(np.dot(dmC, np.linalg.pinv(dmC)),RT1)
-        RT2 = np.array(df.loc[idxI,'RT'])
-        diffI =  RT2 - np.dot(np.dot(dmI, np.linalg.pinv(dmI)),RT2)
-        SSE = np.square(diffC).sum() + np.square(diffI).sum()
-        
-        if (bestSSE <0)|(SSE < bestSSE):
-            bestAlpha = [alpha_face, alpha_name]
-            bestSSE = SSE
-            df_wCP  = df    
-        # end of a subject's modeling
-        
-        
-df_gp_wCP = pd.concat([df_gp_wCP, df_wCP], axis=0)
-gp_alphas.loc[SCNT, 'sbjId']=S
-gp_alphas.loc[SCNT, 'alpha_face']=bestAlpha[0]
-gp_alphas.loc[SCNT, 'alpha_name']=bestAlpha[1]
+            if (bestSSE <0)|(SSE < bestSSE):
+                bestAlpha = [alpha_face, alpha_name]
+                bestSSE = SSE
+                df_wCP  = df    
+    # end of a subject's modeling, put best-fitting CP back to group df and save the best-fitting alphas
+    df_gp_wCP = pd.concat([df_gp_wCP, df_wCP], axis=0)
+    gp_alphas.loc[SCNT, 'sbjId']=S
+    gp_alphas.loc[SCNT, 'alpha_face']=bestAlpha[0]
+    gp_alphas.loc[SCNT, 'alpha_name']=bestAlpha[1]
 
-#%%
-
+# end of all subjects' modeling
+df_gp_wCP.to_pickle('df_wCP.pkl')
 
 
